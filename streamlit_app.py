67702cf737901fce7f0d03c7860a28681533e64b
Raj Tiwari <98082499+rajtiwariee@users.noreply.github.com>
2023-09-15 18:48:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -137,10 +137,15 @@ def load_model():
     return model
 
 
-def load_and_prep(filename, img_shape=224):
-    img = tf.cast(filename, tf.float32)
-    img = tf.image.resize(img, [img_shape, img_shape])
-    return img
+def load_and_prep(image, img_shape=224):
+    # Open and convert the PIL image to a NumPy array
+    image = Image.open(image)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    
+    # Resize the image
+    image = tf.image.resize(image, [img_shape, img_shape])
+    
+    return image
 
 
 def predict(image, model):
