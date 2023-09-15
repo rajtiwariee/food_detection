59420daffb6374ffdf86c008f1bbe58284c26357
Raj Tiwari <98082499+rajtiwariee@users.noreply.github.com>
2023-09-15 18:26:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -132,7 +132,7 @@ CLASS_NAMES = [
 @st.cache(allow_output_mutation=True)
 def load_model():
     model = tf.keras.models.load_model(
-        "models/fine_tuned_model_with_model_ckpt_wo_mp.h5"
+        "model.h5"
     )
     return model
 
